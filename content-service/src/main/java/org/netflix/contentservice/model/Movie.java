e81ef96201e2b1d951f17a5b9b3562022ae580f3
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2026-07-16 19:45:59 +0530

    feat: add creation and update timestamps to Movie entity

--- content-service/src/main/java/org/netflix/contentservice/model/Movie.java
+++ content-service/src/main/java/org/netflix/contentservice/model/Movie.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -49,8 +51,10 @@ public class Movie {
     @Enumerated(EnumType.STRING)
     private VideoStatus videoStatus;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }

e6f692b3e56082a59fdafd4da86e5feea6f6c67d
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2026-07-17 20:21:05 +0530

    feat: add title search method to content repository

--- content-service/src/main/java/org/netflix/contentservice/repository/MovieRepository.java
+++ content-service/src/main/java/org/netflix/contentservice/repository/MovieRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie, String> {
 
     List<Movie> findByGenre(Genre genre);
+    List<Movie> findByTitleContainingIgnoreCase(String title);
 }

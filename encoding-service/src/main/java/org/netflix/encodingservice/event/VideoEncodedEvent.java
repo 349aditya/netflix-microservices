55fc6a5a586776fdd8b086b965ef0a2c7f9d1d59
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2026-07-20 21:16:27 +0530

    feat(encoding): add VideoEncodedEvent

---
+++ encoding-service/src/main/java/org/netflix/encodingservice/event/VideoEncodedEvent.java
@@ -0,0 +1,18 @@
+package org.netflix.encodingservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoEncodedEvent {
+
+    private String movieId;
+    private String hlsUrl;
+    private String masterPlaylistKey;
+    private boolean success;
+    private String errorMessage;
+
+}
